40cbde15bc4e1352467f53c6893a6b9c9607cd88
maha kharrat <maha.kharrat@lab5com.fr>
2022-05-31 15:22:18 +0200

    fix getallbeers call api + pagination size

--- src/components/BeersList/BeersList.jsx
+++ src/components/BeersList/BeersList.jsx
@@ -7,44 +7,43 @@ import useHooks from "../../useHooks";
 import { setPageSize } from "../../store/actions";
 const BeersList = () => {
   const state = useSelector((state) => state);
+  const searchWord = state.searchWord;
   const { getAllBeersCallApi } = useHooks();
-  const getBeers = state
-    ? state.searchWord
-      ? state.filtredBeers
-      : state.allBeers
-    : {};
-  const maxSize = 80;
+  const getBeers = state && state.allBeers;
+  const maxSize = searchWord ? (getBeers.length < 10 ? 1 : 25) : 80;
   const pageSizeOptions = [10, 20, 50, 80];
   const dispatch = useDispatch();
 
   const onShowSizeChange = (page, pageSize) => {
-    console.log("page", page);
-    console.log("pageSize", pageSize);
-    dispatch(setPageSize({ payload: pageSize }));
-    getAllBeersCallApi({ page: page, pageSize: pageSize });
+    dispatch(setPageSize({ payload: { page: page, pageSize: pageSize } }));
+    var params = { page: page, pageSize: pageSize };
+    if (searchWord) {
+      params.beerWord = searchWord;
+    }
+    getAllBeersCallApi(params);
   };
   useEffect(() => {
+    dispatch(setPageSize({ payload: { page: 1, pageSize: 10 } }));
     getAllBeersCallApi({ page: 1, pageSize: 10 });
   }, []);
 
   return (
     <>
-      <div className="beers-list">
+      <div className={`beers-list`}>
         <Suspense fallback={<Spin />}>
           {state && !state.loading && <CardList type="beers"></CardList>}
         </Suspense>
       </div>
-      {state && getBeers && getBeers.length > 0 && (
-        <Pagination
-          className="beers-list-pagination"
-          defaultCurrent={1}
-          total={maxSize}
-          showSizeChanger
-          defaultPageSize={state.pageSize}
-          onChange={onShowSizeChange}
-          pageSizeOptions={pageSizeOptions}
-        />
-      )}
+      <Pagination
+        className="beers-list-pagination"
+        defaultCurrent={1}
+        total={getBeers.length ? maxSize : 1}
+        showSizeChanger
+        defaultPageSize={state.pageSize}
+        onChange={onShowSizeChange}
+        pageSizeOptions={pageSizeOptions}
+        disabled={!getBeers.length}
+      />
     </>
   );
 };
